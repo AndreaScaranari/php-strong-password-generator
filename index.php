2db54a312c0208b1bec421cdec04e837a9ca25f5
AndreaScaranari <andrea.scaranari.93@gmail.com>
2024-02-21 15:50:30 +0100

    basic_graphics

---
+++ index.php
@@ -0,0 +1,37 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Strong Password Generator</title>
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- my css -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header class="text-center py-3 text-white">
+        <h1>Strong Password Generator</h1>
+        <h3>Genera una password sicura</h3>
+    </header>
+    <main>
+        <div class="container">
+            <div class="form-card text-center">
+                <form action="" method="GET" class="py-3">
+                    <div class="input-group py-2 d-flex justify-content-center gap-3 align-items-center">
+                    <label for="p-length">Lunghezza Password</label>
+                    <input type="number" name="p-length" >
+                    </div>
+                    <button class="btn btn-primary">Invia</button>
+                    <button class="btn btn-secondary">Annulla</button>
+                </form>
+
+
+            </div>
+        </div>
+    </main>
+
+    
+</body>
+</html>
