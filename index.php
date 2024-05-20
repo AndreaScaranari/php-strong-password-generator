170e145199b353421b4bb0dfc548e7fab5e5219b
AndreaScaranari <andrea.scaranari.93@gmail.com>
2024-05-20 17:30:40 +0200

    index fixes

--- index.php
+++ index.php
@@ -5,17 +5,19 @@ require __DIR__ . '/logic/functions.php';
 
 // # Data
 $p_len = $_GET['p-length'] ?? '';
+$char_repeat = $_GET['char-repeat'] ?? '';
+$char_exclude = $_GET['char_exclude'] ?? '';
 $password = randomPassword($p_len);
 
-// # Redirect
-if (!empty($p_len)) {
-    // Avviare la sessione
-    session_start();
-    $_SESSION['p_len'] = $p_len; 
-    $_SESSION['password'] = $password;
+// // # Redirect
+// if (!empty($p_len)) {
+//     // Avviare la sessione
+//     session_start();
+//     $_SESSION['p_len'] = $p_len; 
+//     $_SESSION['password'] = $password;
 
-    header ('Location: ./pwresults.php');
-}
+//     header ('Location: ./pwresults.php');
+// }
 
 ?>
 
@@ -47,10 +49,30 @@ if (!empty($p_len)) {
             <!-- riquadro generazione password -->
             <div id="pw-generator" class="form-card text-center">
                 <form action="" method="GET" class="py-3 rounded">
+
+                    <!-- input -->
                     <div class="input-group py-2 d-flex justify-content-center gap-3 align-items-center">
                     <label class="rounded col-2" for="p-length">Lunghezza Password</label>
                     <input class="rounded col-2 ps-2" type="number" name="p-length" min="5" max="20" value="<?= !$p_len ? '5' : "$p_len" ?>">
                     </div>
+
+                    <!-- radio -->
+                    <div class="input-group py-2 d-flex justify-content-center gap-3 align-items-center">
+                        <p>Consenti ripetizione di uno o più caratteri:</p>
+                        <input type="radio" id="yes" name="char_repeat" value="Yes" checked>
+                        <label for="yes">Sì</label>
+                        <input type="radio" id="no" name="char_repeat" value="No">
+                        <label for="no">No</label>
+                    </div>
+
+                    <!-- checkbox -->
+                    <div class="input-group py-2 d-flex justify-content-center gap-3 align-items-center">
+                        <input type="checkbox" id="letters" name="char_exclude" value="letters">
+                        <label for="letters">Lettere</label>
+                        <input type="checkbox" id="numbers" name="char_exclude" value="numbers">
+                        <label for="numbers">Numeri</label>
+                    </div>
+
                     <button class="btn btn-primary">Invia</button>
                     <button class="btn btn-secondary">Annulla</button>
                 </form>
